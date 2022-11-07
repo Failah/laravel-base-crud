317efbfcb218f345260b832b3f55e6e245e4c621
ValerioScharizza <valerio.schiarizza@live.it>
2022-11-07 16:03:27 +0100

    added very basic html and finish

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -8,6 +8,15 @@
     <h1>
         {{ $comic->title }}
     </h1>
+    <div>
+        <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+    </div>
+    <p>
+        {{ $comic->description }}
+    </p>
+    <h4 style="margin-bottom: 60px">
+        {{ $comic->price }}
+    </h4>
 
     <a href="{{ route('comics.index') }}">BACK TO THE COMICS LIST</a>
 @endsection
