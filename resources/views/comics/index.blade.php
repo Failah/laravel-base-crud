b4eca42f0c9becb8f433f51f0c4d2624bbf00e96
ValerioScharizza <valerio.schiarizza@live.it>
2022-11-08 15:08:10 +0100

    added basic css style

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,18 +5,23 @@
 @endsection
 
 @section('content')
-    <div>
+    <div id="new-comic">
         <a href="{{ route('comics.create') }}">Add new comic!</a>
     </div>
 
-    @foreach ($comics as $comic)
-        <div>
-            <a href="{{ route('comics.show', $comic->id) }}"> {{ $comic->title }} </a>
-        </div>
-        <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
-            @csrf
-            @method('DELETE')
-            <input onclick="return confirm('Do you really want to delete this Comic?')" type="submit" value="Delete Comic">
-        </form>
-    @endforeach
+    <div id="comics-list">
+        @foreach ($comics as $comic)
+            <div class="comic-card">
+                <div>
+                    <a href="{{ route('comics.show', $comic->id) }}"> {{ $comic->title }} </a>
+                </div>
+                <form id="delete-comic" action="{{ route('comics.destroy', $comic->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <input onclick="return confirm('Do you really want to delete this Comic?')" type="submit"
+                        value="Delete Comic">
+                </form>
+            </div>
+        @endforeach
+    </div>
 @endsection
