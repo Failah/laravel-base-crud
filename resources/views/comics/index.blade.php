7f1a32a910a5663560c7d2b21c6f0ef2ee999785
ValerioScharizza <valerio.schiarizza@live.it>
2022-11-08 14:33:49 +0100

    created delete comic + route in controller

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -13,5 +13,10 @@
         <div>
             <a href="{{ route('comics.show', $comic->id) }}"> {{ $comic->title }} </a>
         </div>
+        <form action="{{ route('comics.destroy', $comic->id) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <input type="submit" value="Delete Comic">
+        </form>
     @endforeach
 @endsection
